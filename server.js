8e427fc752615d56c1a84d9910bb970119b06682
Alex Bradshaw <alexanderbradshaw5@gmail.com>
2021-06-09 18:52:21 -0400

    updated server js and models

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
-
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'thisisverysecret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -25,7 +23,7 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
