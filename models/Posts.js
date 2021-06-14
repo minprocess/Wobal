f1aa7d0a9565b4ea9fd162f353662edb1be9ba33
Alex Bradshaw <alexanderbradshaw5@gmail.com>
2021-06-14 19:13:10 -0400

    badwords

--- models/Posts.js
+++ models/Posts.js
@@ -1,10 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+var badwordsArray = require('badwords/array');
+
 
 class Posts extends Model {}
-
-// badwords code
-var badwordsArray = require('badwords/array');
 
 Posts.init(
   {
@@ -36,18 +35,30 @@ Posts.init(
   {
     hooks: {
       beforeCreate: async (newPostsData) => {
-        if ((newPostsData.description).includes(badwords))
-        newPostsData.description = await badwordsArray.newPostsData
+        const newPostArray = (((newPostsData.dataValues.description)).split(' ')).map(v => v.toLowerCase());
+        const actualArray = (((newPostsData.dataValues.description)).split(' '))
+        for (let i = 0; i < badwordsArray.length; i++) {
+          for (let a = 0; a < newPostArray.length; a++){
+            if(badwordsArray.includes((newPostArray[a]))) {
+              const badWordIndex = badwordsArray.indexOf(newPostArray[a])
+              const badWord = badwordsArray[badWordIndex]
+              const badWordLength = badWord.length
+              const newPostIndex = newPostArray.indexOf(badWord)
+              actualArray[newPostIndex] = '*'.repeat(eval(badWordLength))
+              newPostsData.dataValues.description = actualArray.join(' ')
+              console.log(`\n\n ${badWord} \n\n`);
+              }
+            }
+          }
+          return newPostsData
       }, 
     },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'posts',
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'posts',
-  }
 );
 
 module.exports = Posts;
