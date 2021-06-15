ee5750ba18d3e51f002763025c71d0f03e8417e4
Bill Pate <wtpate69@hotmail.com>
2021-06-15 12:04:00 -0400

    added button in feed to delete comment

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -85,6 +85,26 @@ router.post('/comment', async (req, res) => {
   }
 });
 
+// DELETE a comment
+router.delete('/comment:id', async (req, res) => {
+  try {
+    const commentData = await Comments.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with that id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // To create a user
 router.post('/newuser', async (req, res) => {
   try {
